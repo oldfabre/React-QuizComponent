d0d528420734dd112d316555e3ebf5394ec4d9c6
oldfabre <olden.fabre@finastra.com>
2022-11-07 18:02:27 +0100

    Next step

--- src/Quiz.js
+++ src/Quiz.js
@@ -5,7 +5,7 @@ let quizData = require('./quiz_data.json')
 
 class Quiz extends Component {
 	render() {
-		return <div className="QuizQuestion">{ quizData.quiz_questions[0].instruction_text }</div>
+		return <QuizQuestion />
 	}
 
 	constructor(props) {
