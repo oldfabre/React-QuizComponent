070bd36e072230ba982d51aa4b33bf8ce003cfb6
oldfabre <olden.fabre@finastra.com>
2022-11-07 17:35:56 +0100

    Next step

--- src/Quiz.js
+++ src/Quiz.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react'
 let quizData = require('./quiz_data.json')
 
 class Quiz extends Component {
-
+	render() {
+		return <div></div>
+	}
 }
 
 export default Quiz
